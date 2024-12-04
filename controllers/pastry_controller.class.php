b60ea477e5c945d0253c5773fc5d7b9648bb4e57
Deirdre <dmleib@iu.edu>
2024-12-03 21:19:14 -0500

    Updated Controller with search, edit, update, add, suggest

--- controllers/pastry_controller.class.php
+++ controllers/pastry_controller.class.php
@@ -40,14 +40,109 @@ class PastryController{
         $view = new PastryIndex();
         $view->display($pastries);
     }
+
+    //display a pastry in a form for editing
+    public function edit($id): void {
+        //retrieve the specific pastry
+        $pastry = $this->pastry_model->get_pastry_by_id($id);
+
+        if (!$pastry) {
+            //display an error
+            $this->error("There was a problem displaying the pastry id='" . $id . "'.");
+            return;
+        }
+
+        $view = new PastryEdit();
+        $view->display($pastry);
+    }
+
+    //update a pastry in the database
+    public function updatePastry($id): void {
+        //update the pastry
+        $update = $this->pastry_model->update_pastry($id);
+        if (!$update) {
+            //handle errors
+            $this->error("There was a problem updating the pastry id='" . $id . "'.");
+            return;
+        }
+
+        //display the updated pastry details
+        $confirm = "The pastry was successfully updated.";
+        $pastry = $this->pastry_model->get_pastry_by_id($id);
+
+        $view = new PastryDetail();
+        $view->display($pastry, $confirm);
+    }
+
+
+    //update a category in the database
+    public function updateCategory($id): void {
+        //update the category
+        $update = $this->pastry_model->update_category($id);
+        if (!$update) {
+            //handle errors
+            $this->error("There was a problem updating the category id='" . $id . "'.");
+            return;
+        }
+
+        //display a confirmation message
+        echo "The category was successfully updated.";
+    }
+
+    //search pastries
+    public function search(): void {
+        //retrieve query terms from search form
+        $query_terms = trim($_GET['query'] ?? "");
+
+        //if search term is empty, list all pastries
+        if ($query_terms === "") {
+            $this->index();
+            return;
+        }
+
+        //search the database for matching pastries
+        $pastries = $this->pastry_model->search_pastries($query_terms);
+
+        if ($pastries === 0) {
+            //handle no results found
+            $this->error("No results found for the search term '$query_terms'.");
+            return;
+        } elseif (!$pastries) {
+            //handle an error during the search
+            $this->error("An error has occurred.");
+            return;
+        }
+
+        //display matched pastries
+        $search = new PastrySearch();
+        $search->display($query_terms, $pastries);
+    }
+
+    //autosuggestion for search terms
+    public function suggest($terms): void {
+        //retrieve query terms
+        $query_terms = urldecode(trim($terms));
+        $pastries = $this->pastry_model->search_pastries($query_terms);
+
+        //retrieve all pastry names and store them in an array
+        $names = array();
+        if ($pastries) {
+            foreach ($pastries as $pastry) {
+                $names[] = $pastry->getName();
+            }
+        }
+
+        echo json_encode($names);
+    }
     //handle an error
     public function error($message):void{
         //create an object of the Error class
         $error = new PastryError();
-
         //display the error page
         $error->display($message);
 
     }
 
+
 }
+
