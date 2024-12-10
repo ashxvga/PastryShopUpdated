d9703dfc80b503cb6fc4314645f63cc0fff127d0
KierraWhit <89669379+KierraWhit@users.noreply.github.com>
2024-12-10 10:33:30 -0500

    Create database_connection_exception.class.php

---
+++ Exceptions/database_connection_exception.class.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Author: Kierra White
+ * Date: 12/10/24
+ * Name: database_connection_exception.class.php
+ * Description: An exception class that handles database connection exceptions. The class extends the Exception class.
+ */
+class DatabaseConnectionException extends Exception{
+    //There is no need of any code.
+}
