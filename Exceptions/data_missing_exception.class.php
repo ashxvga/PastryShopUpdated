e4e5d977a039906c0188c93f471945bc54b86ab5
KierraWhit <89669379+KierraWhit@users.noreply.github.com>
2024-12-10 10:32:52 -0500

    Create data_missing_exception.class.php

---
+++ Exceptions/data_missing_exception.class.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Author: Kierra White
+ * Date: 12/10/2024
+ * Name: data_missing_exception.class.php
+ * Description: An exception class that handles missing data exception. The class extends the Exception class.
+ */
+class DataMissingException extends Exception{
+    //There is no need of any code.
+}
