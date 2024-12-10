1f34d2540bea762bd6bd7a9556b4e4784756a4b4
KierraWhit <89669379+KierraWhit@users.noreply.github.com>
2024-12-10 10:34:18 -0500

    Create database_execution_exception.class.php

---
+++ Exceptions/database_execution_exception.class.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Author: Kierra White
+ * Date: 12/10/24
+ * File: database_execution_exception.class.php
+ * Description: An exception class that handles database execution exceptions. The class extends the Exception class.
+ */
+class DatabaseExecutionException extends Exception {
+    //There is no need of any code.
+}
