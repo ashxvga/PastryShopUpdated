5beffa45536bc7d0984484e554b48d32b1e1b492
KierraWhit <89669379+KierraWhit@users.noreply.github.com>
2024-12-10 10:34:52 -0500

    Create email_exception.class.php

---
+++ Exceptions/email_exception.class.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Author: Kierra White
+ * Date:12/10/2024
+ * Name: email_exception.class.php
+ * Description: An exception class that handles invalid email address. The class extends the Exception class.
+ */
+class InvalidDateException extends Exception{
+    //There is no need of any code
+}
