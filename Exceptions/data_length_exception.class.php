beb0927f7ac202a26eb398ae40fd6b8d7159fa19
KierraWhit <89669379+KierraWhit@users.noreply.github.com>
2024-12-10 10:36:28 -0500

    Create data_length_exception.class.php

---
+++ Exceptions/data_length_exception.class.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Author: Kierra White
+ * Date: 12/10/*24
+ * File: data_length_exception.class.php
+ * Description: An exception class that handles data length exception. The class extends the Exception class.
+ */
+class DataLengthException extends Exception {
+    //There is no need of any code
+}
