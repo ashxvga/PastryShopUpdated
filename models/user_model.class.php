b4830276641689cde6014f75cb52975d09d2d4c9
KierraWhit <89669379+KierraWhit@users.noreply.github.com>
2024-12-12 09:13:53 -0500

    Update user_model.class.php

--- models/user_model.class.php
+++ models/user_model.class.php
@@ -5,70 +5,61 @@
  *File: user_model.class.php
  *Description: This class handles interaction with the user table in the database
  */
-class UserModel{
+class UserModel
+{
     private Database $db; // Datbase Object
     private mysqli $dbConnection; // Database Connection Object
     private string $tblUsers; //table name
 
     //constructor
-    public function __construct(){
+    public function __construct()
+    {
         $this->db = Database::getDatabase();
         $this->dbConnection = $this->db->getConnection();
         $this->tblUsers = $this->db->getUsersTable();
     }
+
     //Method to add user
-    public function add_user(): bool {
-        $username = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST ['username'])));
-        $password = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST ['password_hash'])));
-        $email = $this->dbConnection-> real_escape_string(trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL)));
-        $firstName = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST ['first_name'])));
-        $lastName = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST ['last_name'])));
-        $role = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST ['role'] ))) ?: 'customer';
+    public function add_user(string $username, $password, $email, $firstName, $lastName, $role = 'customer'): bool
+    {
+        $username = $this->dbConnection->real_escape_string($username);
+        $password = $this->dbConnection->real_escape_string($password);
+        $email = $this->dbConnection->real_escape_string($email);
+        $firstName = $this->dbConnection->real_escape_string($firstName);
+        $lastName = $this->dbConnection->real_escape_string($lastName);
+        $role = $this->dbConnection->real_escape_string($role);
 
-        try {
-            //Handle data missing exeception. All fields are required
-            if (empty ($username) || empty ($password) || empty ($email) || empty ($firstName) ||empty ($lastName)){
-                throw new DataMissingExeception ("Values were missing in one or more fields. All fields must be filled.");
-            }
-            //Handle data length excpection. The min length of a password is 5
-            if (strlen($password) < 5){
-                throw new DataLengthException ("Your password was invalid. The mininum length of a password is 5.");
-            }
-            //Handle email format exeception.
-            if (!Utilities::checkemail ($email)) {
-                throw new EmailFormatException ("Your email format was invalid. The general format of an email address is user@example.com");
-            }
-            //Hash the password
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            //Check if username or email exists
-            $check = "SELECT * FROM $this->tblUsers WHERE username = '$username' OR email = '$email'";
-            $result = $this->dbConnection->query($check);
-            //SQL INSERT statement
-            $sql = " INSERT INTO $this->tblUsers (username, password_hash, email, first_name, last_name, role)
-            VALUES ('$username', '$hashed_password', '$email', '$firstName', '$lastName', '$role')";
-            if ( $this->dbConnection->query($sql) === FALSE) {
-                throw new DatabaseException("We are sorry, but we cann create your accout at this moment. Please try again later.");
-            }
-
-            return "Your account has been successfully created.";
-        } catch (DataMissingException $e) {
-            return $e->getMessage();
-        } catch (DataLengthException $e) {
-            return $e->getMessage();
-        } catch (DatabaseException $e) {
-            return $e->getMessage();
-        } catch (EmailFormatException $e) {
-            return $e->getMessage();
-        } catch (Exception $e) {
-            return $e->getMessage();
+        //Handle data length excpection. The min length of a password is 5
+        if (strlen($password) < 5) {
+            throw new DataLengthException ("Your password was invalid. The mininum length of a password is 5.");
         }
+
+        //Hash the password
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        //Check if username or email exists
+        $check = "SELECT * FROM $this->tblUsers WHERE username = '$username' OR email = '$email'";
+        $result = $this->dbConnection->query($check);
+        if ($check && $check->num_rows > 0){
+        throw new DatabaseException("Username or email already exists.");
+    }
+        
+        //SQL INSERT statement
+        $sql = " INSERT INTO $this->tblUsers (username, password_hash, email, first_name, last_name, role)
+            VALUES ('$username', '$hashed_password', '$email', '$firstName', '$lastName', '$role')";
+        
+        if ($this->dbConnection->query($sql) === FALSE) {
+            throw new DatabaseException("We are sorry, but we cannot create your accout at this moment. Please try again later.");
+        }
+        return "Your account has been successfully created.";
     }
 
 
     //Method to delete user
-    public function delete_user ($userId): bool {
+    public function delete_user(int $userId): bool
+    {
         //Santize
-        $userId = $this->dbConnection->real_escape_string(trim(filter_var($userId, FILTER_VALIDATE_INT)));
+        $userId = $this->dbConnection->real_escape_string($userId);
 
         //SQL DELETE STATEMENT
         $sql = "DELETE FROM $this->tblUsers WHERE user_id = $userId";
@@ -79,99 +70,85 @@ class UserModel{
     }
 
     // Method to verify user
-    public function verify_user($username, $password): bool {
-        try {
-            // Sanitize
-            $username = $this->dbConnection->real_escape_string($username);
-            //$password = $this->dbConnection->real_escape_string($password);
+    public function verify_user($username, $password): bool
+    {
+        // Sanitize
+        $username = $this->dbConnection->real_escape_string($username);
+        //$password = $this->dbConnection->real_escape_string($password);
 
-             // SQL SELECT statement
-            $sql = "SELECT password_hash FROM $this->tblUsers WHERE username = '$username'";
+        // SQL SELECT statement
+        $sql = "SELECT password_hash FROM $this->tblUsers WHERE username = '$username'";
 
-            // Execute query
-            $result = $this->dbConnection->query($sql);
+        // Execute query
+        $result = $this->dbConnection->query($sql);
 
-            // Check if user exists
-            if (!$result || $result->num_rows === 0) {
-                throw new DatabaseExecutionException ("Invalid username or password."); }
+        // Check if user exists
+        if (!$result || $result->num_rows === 0) {
+            throw new DatabaseExecutionException ("Invalid username or password.");
+        }
 
-            $row = $result->fetch_assoc();
-            $hashed_password = $row['password_hash'];
+        $row = $result->fetch_assoc();
+        $hashed_password = $row['password_hash'];
 
-            // Verify the password
-            if (!password_verify($password, $hashed_password)) {
-                throw new DatabaseExecutionException ("Invalid username or password."); }
+        // Verify the password
+        if (!password_verify($password, $hashed_password)) {
+            throw new DatabaseExecutionException ("Invalid username or password.");
+        }
 
-            // Set cookie
-            setcookie("user", $username, time() + 3600, "/");
-            return true;
-        } catch (DatabaseExecutionException  $e) {
-            echo $e->getMessage();
-            return false;
-        } catch (Exception $e) {
-            echo "An unexpected error occurred: " . $e->getMessage();
-            return false;
-    }
-}
-
-            
-    //Method to log user out
-    public function logout(): bool{
-        //destroy session data
-        setcookie("user", "", time() -3600, "/");
+        // Set cookie
+        setcookie("user", $username, time() + 3600, "/");
         return true;
     }
-        
+
+
+    //Method to log user out
+    public function logout(): bool
+    {
+        //destroy session data
+        setcookie("user", "", time() - 3600, "/");
+        return true;
+    }
+
 
     //Method to reset user's password
-    public function reset_password():bool{
-        $username = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST ['username'])));
-        $password = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST, ['password_hash'])));;
-        try {
-            if (empty($username) || empty($password)) {
-                throw new DataMissingException("Values were missing in one or more fields. All fields must be filled.");
-            }
+    public function reset_password($username, $password): bool
+    {
+        $username = $this->dbConnection->real_escape_string($username);
+        //$password = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST, ['password_hash'])));;
 
-            //Handle data length exception. The min length of a password is 5.
-            if (strlen($password) < 5) {
-                throw new DataLengthException("Your password is invalid. The mininum length of a password is 5.");
-            }
-            //Hash the password
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            //SQL UPDATE statement
-            $sql = "UPDATE $this->tblUsers SET password_hash = '$hashed_password' WHERE username = '$username'";
-            //execute the query
-            $query = $this->dbConnection->query($sql);
-
-            //return false if no rows were affected
-            if (!$query || $this->dbConnection->affected_rows == 0) {
-                throw new DatabaseException("We are sorry, but we cannot reset your password at this moment. Please try again later.");
-            }
-            return "You have successfully reset your password.";
-        } catch (DataMissingException $e) {
-            return $e->getMessage();
-        } catch (DataLengthException $e) {
-            return $e->getMessage();
-        } catch (DatabaseException $e) {
-            return $e->getMessage();
-        } catch (Exception $e) {
-            return $e->getMessage();
+        //Handle data length exception. The min length of a password is 5.
+        if (strlen($password) < 5) {
+            throw new DataLengthException("Your password is invalid. The mininum length of a password is 5.");
         }
+        //Hash the password
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        //SQL UPDATE statement
+        $sql = "UPDATE $this->tblUsers SET password_hash = '$hashed_password' WHERE username = '$username'";
+        //execute the query
+        $query = $this->dbConnection->query($sql);
+
+        //return false if no rows were affected
+        if (!$query || $this->dbConnection->affected_rows == 0) {
+            throw new DatabaseException("We are sorry, but we cannot reset your password at this moment. Please try again later.");
+        }
+        return "You have successfully reset your password.";
     }
 
     //Method to update user
-    public function update_user ($userId) : bool{
+    public function update_user(int $userId, string $username, $email, $firstName, $lastName): bool
+    {
         if (!filter_has_var(INPUT_POST, 'username') ||
-            !filter_has_var(INPUT_POST,'email') ||
-            !filter_has_var(INPUT_POST,'firstName') ||
-            !filter_has_var(INPUT_POST, 'lastName') )
-        {
+            !filter_has_var(INPUT_POST, 'email') ||
+            !filter_has_var(INPUT_POST, 'firstName') ||
+            !filter_has_var(INPUT_POST, 'lastName')) {
             return false;
         }
-        $username = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST ['username'])));
-        $email = $this->dbConnection-> real_escape_string(trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL)));
-        $firstName = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST, ['first_name'])));
-        $lastName = $this->dbConnection-> real_escape_string(trim(htmlspecialchars($_POST, ['last_name'])));
+        $userId = $this->dbConnection->real_escape_string($userId);
+        $username = $this->dbConnection->real_escape_string($username);
+        $email = $this->dbConnection->real_escape_string($email);
+        $firstName = $this->dbConnection->real_escape_string($firstName);
+        $lastName = $this->dbConnection->real_escape_string($lastName);
 
         //SQL UPDATE statement
         $sql = "UPDATE $this->tblUsers 
@@ -180,7 +157,4 @@ class UserModel{
 
         return $this->dbConnection->query($sql) === true;
     }
-
-
-
 }
