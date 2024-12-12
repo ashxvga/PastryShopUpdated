113de80473006ebc0e8d853b8f20a4344c4daf28
Ashley Rodriguez Vega <150083400+ashxvga@users.noreply.github.com>
2024-12-12 13:24:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,15 @@
  * File: index.php
  * Description: The homepage
  */
+
 //load application settings
+
 require_once ("application/config/config.php");
 //load autoloader
 require_once ("vendor/autoload.php");
-
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+var_dump($_SESSION);
 //load the dispatcher that dissects a request URL
 new Dispatcher();
