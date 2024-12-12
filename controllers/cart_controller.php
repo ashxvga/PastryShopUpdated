a80eda3f02578f38c101e29eaf0cbec195b5c1aa
Ashley Rodriguez Vega <150083400+ashxvga@users.noreply.github.com>
2024-12-12 11:48:04 -0500

    Update cart_controller.php

--- controllers/cart_controller.php
+++ controllers/cart_controller.php
@@ -5,6 +5,7 @@
  * File: cart_controller.class.php
  * Description: A controller for interacting with the CartModel.
  */
+session_start();
 class CartController
 {
     private CartModel $cartModel;
@@ -15,11 +16,20 @@ class CartController
         $this->cartModel = new CartModel();
     }
 
+    public function index(): void
+    {
+        $cartItems = $this->cartModel->get_cart();
+        // Display the user dashboard view
+        $dashboardView = new CartView();
+        $dashboardView->display($cartItems);
+    }
+
     // Add a pastry to the cart
     public function addToCart($pastryId): void
     {
         $this->cartModel->add_cart($pastryId);
-        echo "Pastry with ID $pastryId has been added to the cart.\n";
+       // echo "Pastry with ID $pastryId has been added to the cart.\n";
+        header('Location: ' . BASE_URL . '/cart/index');
     }
 
     // Get all items in the cart
@@ -30,9 +40,11 @@ class CartController
             echo "The cart is empty.\n";
         } else {
             echo "Items in your cart:\n";
-            foreach ($cartItems as $item) {
-                echo "- Pastry ID: $item\n";
-            }
+            //foreach ($cartItems as $item) {
+            //echo "- Pastry ID: $item\n";
+            $display = new CartView();
+            $display->display($cartItems);
         }
     }
 }
+
